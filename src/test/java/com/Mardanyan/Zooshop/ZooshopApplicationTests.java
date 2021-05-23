37209ac492973bfb163d51b5efbefbbb5d644a59
Artyom <arkhamb@yandex.ru>
2021-05-24 01:31:13 +0300

    добавление базовой настройки spring, добавление контроллеров, моделей для отображения, шаблонов html файла

---
+++ src/test/java/com/Mardanyan/Zooshop/ZooshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mardanyan.Zooshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
