01b677e555576cd1d34fb5e25a80ca72b5581558
Artyom <arkhamb@yandex.ru>
2021-05-25 16:49:10 +0300

    Изменили контроллер с добавлением страницы к animals

--- src/main/java/com/Mardanyan/Zooshop/controllers/MainController.java
+++ src/main/java/com/Mardanyan/Zooshop/controllers/MainController.java
@@ -1,11 +1,26 @@
 package com.Mardanyan.Zooshop.controllers;
 
+import com.Mardanyan.Zooshop.models.Product;
+import com.Mardanyan.Zooshop.models.ProductType;
+import com.Mardanyan.Zooshop.repository.ProductRepository;
+import com.Mardanyan.Zooshop.repository.ProductTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class MainController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductTypeRepository productTypeRepository;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -13,4 +28,21 @@ public class MainController {
         return "home";
     }
 
+
+    //Страница с каталогами
+    @GetMapping("/animals")
+    public String animals(Model model){
+
+        /**
+         * Map в котором для каждого типа продукта хранится свой список продуктов
+         */
+        Iterable<ProductType> types =productTypeRepository.findAll();
+        Map<ProductType, List<Product>> map = new HashMap<>();
+        // Идет по каждому типу и для каждого типа в MAP закидывается ключ и значение.
+        types.forEach(type -> map.put(type, productRepository.findByProductType(type)));
+        model.addAttribute("map", map);
+        return "animals";
+    }
 }
+
+
