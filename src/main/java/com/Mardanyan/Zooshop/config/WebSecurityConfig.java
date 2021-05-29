eede53d3c705d84109edd0bc2f8a61f269d6fd6f
Artyom <arkhamb@yandex.ru>
2021-05-29 04:04:15 +0300

    добавление аутентификации spring security

---
+++ src/main/java/com/Mardanyan/Zooshop/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.Mardanyan.Zooshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("pass")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder encoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
